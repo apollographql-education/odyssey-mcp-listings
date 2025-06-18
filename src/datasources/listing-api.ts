68cd4f67559a51b6f0a1d625a3a39e429a046e48
Liz Hennessy <lizhennessy@MacBookPro.hsd1.ut.comcast.net>
2025-06-18 13:48:21 -0600

    update url

--- src/datasources/listing-api.ts
+++ src/datasources/listing-api.ts
@@ -2,7 +2,7 @@ import { RESTDataSource } from "@apollo/datasource-rest";
 import { Listing, Amenity, CreateListingInput } from "../types";
 
 export class ListingAPI extends RESTDataSource {
-  baseURL = "https://rt-airlock-services-listing.herokuapp.com/";
+  baseURL = "https://airlock-listings.demo-api.apollo.dev/";
 
   getFeaturedListings(): Promise<Listing[]> {
     return this.get<Listing[]>("featured-listings");
